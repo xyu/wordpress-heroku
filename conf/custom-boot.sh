9025754f7b19907ecec2d997281c825845c6d5d5
Xiao Yu <me@xyu.io>
2013-07-28 17:02:20 -0400

    Add if to only set ssl certs if they exist

--- conf/custom-boot.sh
+++ conf/custom-boot.sh
@@ -2,13 +2,16 @@
 echo "Include /app/www/conf/httpd.conf" >> /app/apache/conf/httpd.conf
 
 # Write certs in env to files and replace with path
-mkdir "/app/www/certs"
-echo "$KEY_PEM" > /app/www/certs/key.pem
-echo "$CERT_PEM" > /app/www/certs/cert.pem
-echo "$CA_PEM" > /app/www/certs/ca.pem
-export KEY_PEM="/app/www/certs/key.pem"
-export CERT_PEM="/app/www/certs/cert.pem"
-export CA_PEM="/app/www/certs/ca.pem"
+if [ -n "$KEY_PEM" -a -n "$CERT_PEM" -a -n "$CA_PEM" ]
+then
+  mkdir "/app/www/certs"
+  echo "$KEY_PEM" > /app/www/certs/key.pem
+  echo "$CERT_PEM" > /app/www/certs/cert.pem
+  echo "$CA_PEM" > /app/www/certs/ca.pem
+  export KEY_PEM="/app/www/certs/key.pem"
+  export CERT_PEM="/app/www/certs/cert.pem"
+  export CA_PEM="/app/www/certs/ca.pem"
+fi
 
 # Heroku boot.sh
 for var in `env|cut -f1 -d=`; do
