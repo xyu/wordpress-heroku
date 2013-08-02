ae222e88402a179112f3a7e14246d3736eeeb54d
Xiao Yu <me@xyu.io>
2013-08-02 18:50:31 -0400

    Force previews to go over SSL as our logged in cookies are over SSL

--- htdocs/wp-content/mu-plugins/ssl-domain-alias.php
+++ htdocs/wp-content/mu-plugins/ssl-domain-alias.php
@@ -10,33 +10,67 @@
  * @package SSL_Domain_Alias
  */
 
-/**
- * Swap out the current site domain with {@see SSL_DOMAIN_ALIAS} if the
- * protocol is HTTPS.
- *
- * This function is not bulletproof, and expects {@see SSL_DOMAIN_ALIAS} to
- * be defined.
- *
- * @todo The replacement is a simple string replacement (for speed). If the
- * domain name is matching other parts of the URL other than the host, we'll
- * need to switch to a more rigid regex.
- *
- * @param string $url
- * @return string
- */
-function _use_ssl_domain_alias_for_https( $url ) {
-    static $domain;
-    if ( ! isset( $domain ) )
-        $domain = ( defined( 'SSL_DOMAIN_ALIAS' ) && '' != SSL_DOMAIN_ALIAS ) ? parse_url( get_option( 'siteurl' ), PHP_URL_HOST ) : false;
+class wp_plugin_ssl_domain_alias {
+	private $_http_domain;
+	private $_https_domain;
 
-    if ( $domain && 0 === strpos( $url, 'https' ) )
-        $url = str_replace( $domain, SSL_DOMAIN_ALIAS, $url );
+	public function __construct($http_domain, $https_domain) {
+		$this->_http_domain = $http_domain;
+		$this->_https_domain = $https_domain;
+	}
 
-    return $url;
+	/**
+	 * Swap out the current site domain with {@see SSL_DOMAIN_ALIAS} if the
+	 * protocol is HTTPS.
+	 *
+	 * This function is not bulletproof, and expects {@see SSL_DOMAIN_ALIAS} to
+	 * be defined.
+	 *
+	 * @todo The replacement is a simple string replacement (for speed). If the
+	 * domain name is matching other parts of the URL other than the host, we'll
+	 * need to switch to a more rigid regex.
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	public function use_secure_domain_for_https( $url ) {
+	    if ( 0 === strpos( $url, 'https://' ) )
+		$url = str_replace( 'https://'.$domain, 'https://'.SSL_DOMAIN_ALIAS, $url );
+
+	    return $url;
+	}
+
+	/**
+	 * Swap out the current site domain and scheme with {@see SSL_DOMAIN_ALIAS} if the
+	 * protocol is HTTP.
+	 *
+	 * This function is not bulletproof, and expects {@see SSL_DOMAIN_ALIAS} to
+	 * be defined.
+	 *
+	 * @todo The replacement is a simple string replacement (for speed). If the
+	 * domain name is matching other parts of the URL other than the host, we'll
+	 * need to switch to a more rigid regex.
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	public function use_secure_domain_for_http( $url ) {
+	    if ( 0 === strpos( $url, 'http://' ) )
+		$url = str_replace( 'http://'.$domain, 'https://'.SSL_DOMAIN_ALIAS, $url );
+
+	    return $url;
+	}
 }
 
-add_filter( 'plugins_url', '_use_ssl_domain_alias_for_https', 1 );
-add_filter( 'content_url', '_use_ssl_domain_alias_for_https', 1 );
-add_filter( 'site_url', '_use_ssl_domain_alias_for_https', 1 );
-add_filter( 'preview_post_link', '_use_ssl_domain_alias_for_https', 1 );
+if ( defined( 'SSL_DOMAIN_ALIAS' ) && '' != SSL_DOMAIN_ALIAS ) {
+	$wp_plugin_ssl_domain_alias = new wp_plugin_ssl_domain_alias(
+		parse_url( get_option( 'siteurl' ), PHP_URL_HOST ),
+		SSL_DOMAIN_ALIAS
+	);
+
+	add_filter( 'plugins_url', array( $wp_plugin_ssl_domain_alias, 'use_secure_domain_for_https' ), 1 );
+        add_filter( 'content_url', array( $wp_plugin_ssl_domain_alias, 'use_secure_domain_for_https' ), 1 );
+        add_filter( 'site_url', array( $wp_plugin_ssl_domain_alias, 'use_secure_domain_for_https' ), 1 );
+	add_filter( 'preview_post_link', array( $wp_plugin_ssl_domain_alias, 'use_secure_domain_for_http' ), 1 );
+}
 
