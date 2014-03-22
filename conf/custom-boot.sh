6bbf455b4b5a99acca7f279e92fb47eb6254d4fb
Xiao Yu <me@xyu.io>
2014-03-22 16:36:42 -0400

    Build WP site on deploy

--- conf/custom-boot.sh
+++ conf/custom-boot.sh
@@ -1,6 +1,15 @@
 # Add include for overrides
 echo "Include /app/www/conf/httpd.conf" >> /app/apache/conf/httpd.conf
 
+# Build the release
+echo "Building release"
+mkdir /app/www/htdocs
+cp -R /app/www/WordPress/* /app/www/htdocs/.
+cp -R /app/www/site_overwrites/* /app/www/htdocs/.
+cp -R /app/www/site_overwrites/.* /app/www/htdocs/.
+rm -rf /app/www/WordPress
+rm -rf /app/www/site_overwrites
+
 # Heroku boot.sh
 for var in `env|cut -f1 -d=`; do
   echo "PassEnv $var" >> /app/apache/conf/httpd.conf;
